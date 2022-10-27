c9c2de385858b3a66e8d23176d831dca5391348d
Jack Dabrowski <jack.dabrowski@crunch.co.uk>
2022-10-27 11:19:41 +0100

    Update

--- script.py
+++ script.py
@@ -1,4 +1,12 @@
+import os
+
 with open("document1") as f:
     data = f.read()
+
+stream = os.popen("git tag")
+while True:
+    line = stream.readline()
+    if not line: break
+    print(line)
 
 print(data)
